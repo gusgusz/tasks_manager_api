ed9f82ce4eb47302a79c5b31cc8ef9640a9b1b3c
gusgusz <gustavoguimaraescamps@gmail.com>
2023-01-23 12:36:14 -0300

    feat: delete and updateTasks

--- src/controllers/roomControllers.ts
+++ src/controllers/roomControllers.ts
@@ -129,3 +129,101 @@ export async function getTasks(req: Request, res: Response) {
         return;
     }
 }
+
+export async function deleteTask(req: Request, res: Response) {
+    const id : number = res.locals.id;
+    const roomId : number = Number(req.params.id);
+    const taskId : number = Number(req.params.taskId);
+    try {
+        const isRoom = await connectionDb.query(`SELECT * FROM rooms WHERE id = $1;`, [roomId]);
+        if (isRoom.rowCount === 0) {
+            res.sendStatus(404);
+            return;
+        }
+       
+        const isTask = await connectionDb.query(`SELECT * FROM tasks WHERE id = $1;`, [taskId]);
+        if (isTask.rowCount === 0) {
+            res.sendStatus(404);
+            return;
+        }
+
+        if (isRoom.rows[0].userId !== id || isTask.rows[0].userId !== id) {
+            res.sendStatus(401);
+            return;
+        }
+        await connectionDb.query(`DELETE FROM tasks WHERE id = $1;`, [taskId]);
+        res.sendStatus(200);
+        return;
+    } catch (err) {
+        res.sendStatus(500);
+        return;
+    }
+}
+
+export async function updateTask(req: Request, res: Response) {
+    const id : number = res.locals.id;
+    const roomId : number = Number(req.params.id);
+    const taskId : number = Number(req.params.taskId);
+    const validation = taskSchema.validate(req.body);
+    if (validation.error) {
+        res.status(400).send(validation.error.details[0].message);
+        return;
+    }
+    const { nameTask, email, dueDate, isDone} = req.body;
+    try {
+        const isRoom = await connectionDb.query(`SELECT * FROM rooms WHERE id = $1;`, [roomId]);
+        if (isRoom.rowCount === 0) {
+            res.sendStatus(404);
+            return;
+        }
+        const isEmail = await connectionDb.query(`SELECT * FROM users WHERE email = $1;`, [email]);
+        if (isEmail.rowCount === 0) {
+            res.status(400).send('Email not found');
+            return;
+        }
+        const userId = isEmail.rows[0].id;
+        const isTask = await connectionDb.query(`SELECT * FROM tasks WHERE id = $1;`, [taskId]);
+        if (isTask.rowCount === 0) {
+            res.sendStatus(404);
+            return;
+        }
+        if (isRoom.rows[0].userId !== id || isTask.rows[0].userId !== id) {
+            res.sendStatus(401);
+            return;
+        }
+        await connectionDb.query(`UPDATE tasks SET "nameTask" = $1, "userId" = $2, "dueDate" = $3, "isDone" = $4 WHERE id = $5;`, [nameTask, userId, dueDate, isDone, taskId]);
+        res.sendStatus(200);
+        return;
+    } catch (err) {
+        res.sendStatus(500);
+        return;
+    }
+}
+
+export async function updateTaskDone(req: Request, res: Response) {
+    const id : number = res.locals.id;
+    const roomId : number = Number(req.params.id);
+    const taskId : number = Number(req.params.taskId);
+    try {
+        const isRoom = await connectionDb.query(`SELECT * FROM rooms WHERE id = $1;`, [roomId]);
+        if (isRoom.rowCount === 0) {
+            res.sendStatus(404);
+            return;
+        }
+        const isTask = await connectionDb.query(`SELECT * FROM tasks WHERE id = $1;`, [taskId]);
+        if (isTask.rowCount === 0) {
+            res.sendStatus(404);
+            return;
+        }
+        if (isRoom.rows[0].userId !== id || isTask.rows[0].userId !== id) {
+            res.sendStatus(401);
+            return;
+        }
+        await connectionDb.query(`UPDATE tasks SET "isDone" = NOT "isDone" WHERE id = $1;`, [taskId]);
+        res.sendStatus(200);
+        return;
+    } catch (err) {
+        res.sendStatus(500);
+        return;
+    }
+}
--- src/routes/roomRouters.ts
+++ src/routes/roomRouters.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createRoom, getRooms, deleteRoom, createTask, getTasks } from "../controllers/roomControllers.js";
+import { createRoom, getRooms, deleteRoom, createTask, getTasks, deleteTask, updateTask, updateTaskDone } from "../controllers/roomControllers.js";
 import { authMiddleware } from "../middlewares/auth.middlewares.js";
 
 
@@ -11,6 +11,9 @@ router.get('/room', getRooms);
 router.delete('/room/:id', deleteRoom);
 router.post('/room/:id', createTask);
 router.get('/room/:id', getTasks);
+router.delete('/room/:id/:taskId', deleteTask)
+router.put('/room/:id/:taskId', updateTask)
+router.put('/room/:id/:taskId/done', updateTaskDone)
 
 
 export default router;
